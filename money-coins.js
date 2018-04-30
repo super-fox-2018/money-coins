08adc404d93b33d5bb877e50a8db267fd95ee301
Rin Ng <Rin08.ng@gmail.com>
2018-04-30 17:51:05 +0700

    Added finished code for money coins

---
+++ money-coins.js
@@ -0,0 +1,39 @@
+function convertToCoin (money) {
+
+  var coin = [1,10,20,50,100,200,500,1000, 2000, 5000, 10000];
+  var arr = [];
+
+
+
+   do{
+    for(var i = 0; i < coin.length; i++){
+      if(money === coin[i]){
+      arr.push(coin[i]);
+      money -= coin[i];
+      }
+
+      if((money - coin[i-1]) >= 0 && (money - coin[i]) < 0){
+      arr.push(coin[i-1]);
+      money -= coin[i-1];
+    }
+
+    if((money - coin[coin.length -1]) >= 0){
+      arr.push(coin[coin.length - 1]);
+      money -= coin[coin.length - 1];
+    }
+    }
+   }while(money > 0)
+
+   return arr;
+
+}
+
+// Drive code
+console.log(convertToCoin(543))
+//output : [ 500, 20, 20, 1, 1, 1 ]
+
+console.log(convertToCoin(7752))
+//output : [ 5000, 2000, 500, 200, 50, 1, 1 ]
+
+console.log(convertToCoin(37454))
+//output : [ 10000, 10000, 10000, 5000, 2000, 200, 200, 50, 1, 1, 1, 1 ]
