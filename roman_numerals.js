b8d377f3d0d15acd3f32f60fd197a46aae80c27d
ridozaen <ridozaen@gmail.com>
2018-04-30 17:54:56 +0700

    add money coins task

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,23 @@
-function convertToCoin (money) {
+function convertToCoin(money) {
   // your implementation code here
+  const pecahan = [1, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000];
+  // const pecahan = [10000]
+  pecahan.sort(function (a, b) {
+    return (b - a);
+  })
+  arrPecahan = [];
+  sisa = 0;
+  while (money > 0) {
+
+    for (var i = 0; i < pecahan.length; i++) {
+      if (money >= pecahan[i]) {
+        money = money - pecahan[i];
+        arrPecahan.push(pecahan[i]);
+        break;
+      }
+    }
+  }
+  return arrPecahan;
 }
 
 // Drive code
